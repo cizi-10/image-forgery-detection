07948a9f5bf2cd1a938156c8999f9c7f25b1c183
cizi-10 <tapai6789@gmail.com>
2024-06-15 02:38:49 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,12 +4,6 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing.image import img_to_array
 import matplotlib.pyplot as plt
 from PIL import Image
-import gdown
-import os
-
-# Function to download the model from Google Drive
-def download_model(drive_url, output_path):
-    gdown.download(drive_url, output_path, quiet=False)
 
 # Function to preprocess the input image
 def preprocess_image(image_path, target_size=(512, 512)):
@@ -23,8 +17,7 @@ def preprocess_image(image_path, target_size=(512, 512)):
 # Function to make predictions
 def predict(image_path, model):
     image = preprocess_image(image_path)
-    predictions = model(image)
-    predictions = predictions.numpy()  # Adjust based on model's output
+    predictions = model.predict(image)
     return predictions
 
 # Streamlit app
@@ -33,25 +26,11 @@ st.title("Image Forgery Detection")
 # Upload image
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
-# Google Drive link to the model and output path
-drive_url = 'https://drive.google.com/drive/folders/1KUzAPhuWDkSGVBwnGRIucoe34c-6bqWA?usp=sharing'
-model_dir = 'SavedModel'
-output_path = f'{model_dir}.zip'
-
-# Download and extract model if not already present
-if not os.path.exists(model_dir):
-    try:
-        download_model(drive_url, output_path)
-        tf.keras.utils.get_file(fname=model_dir, origin=f'file://{output_path}', untar=True)
-        st.success("Model downloaded and extracted successfully!")
-    except Exception as e:
-        st.error(f"Error downloading the model: {e}")
-
 # Load model
+model_path = "model_unet_2.keras"  # Update this with the correct path to your .keras model
+
 try:
-    saved_model = tf.saved_model.load(model_dir)
-    infer = saved_model.signatures['serving_default']  # Load the serving signature
-    model = tf.keras.layers.Lambda(lambda x: infer(tf.constant(x))['output_0'])  # Wrap in a Keras layer
+    model = tf.keras.models.load_model(model_path)
     st.success("Model loaded successfully!")
 except Exception as e:
     st.error(f"Error loading the model: {e}")
